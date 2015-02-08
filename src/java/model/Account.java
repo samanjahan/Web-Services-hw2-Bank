437bbe56f01a622458171750d6b11ba3bd295f93
Saman Jahanbakhsh <samanj@kth.se>
2015-02-08 19:15:18 +0100

    add web services

---
+++ src/java/model/Account.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author syst3m
+ */
+public class Account {
+    
+    private String personName;
+    private String creditCard;
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public String getCreditCard() {
+        return creditCard;
+    }
+
+    public void setCreditCard(String creditCard) {
+        this.creditCard = creditCard;
+    }
+    
+}
---
+++ src/java/model/Bank.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author syst3m
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Account;
+public class Bank {
+    
+    List<Account> accountList = new ArrayList<Account>();  
+    
+    public Bank(){
+        Account ac1 = new Account();
+        Account ac2 = new Account();
+        Account ac3 = new Account();
+
+        ac1.setPersonName("saman");
+        ac1.setCreditCard("123456");
+        accountList.add(ac1);
+        
+        ac2.setPersonName("alex");
+        ac2.setCreditCard("1234567");
+        accountList.add(ac2);
+        
+        ac3.setPersonName("Jax");
+        ac3.setCreditCard("123456");
+        accountList.add(ac3);       
+    }
+    
+    public boolean pay(String creditCard, String personName){
+        for(int  i = 0; i< accountList.size(); ++i){
+            if(accountList.get(i).getPersonName().equals(personName) && accountList.get(i).getCreditCard().equals(creditCard)){
+                return true;
+            }           
+        }
+        return false;
+    }
+    
+}
