a28ee536e2ed1103bc4f88a70d33665498757505
alexgzb <alexgh@kth.se>
2015-02-09 06:13:20 +0100

    Done some changes to the ws

--- src/java/bank/BankServices.java
+++ src/java/bank/BankServices.java
@@ -20,11 +20,15 @@ import javax.ejb.Stateless;
 public class BankServices {
 
     /**
-     * This is a sample web service operation
+     * 
+     * @param personName
+     * @param creditCard
+     * @param amount
+     * @return 
      */
     @WebMethod(operationName = "pay")
-    public boolean pay(@WebParam(name = "personName") String personName,@WebParam(name = "creditCard") String creditCard) {
+    public boolean pay(@WebParam(name = "personName") String personName,@WebParam(name = "creditCard") String creditCard, @WebParam(name = "amount") double amount) {
         Bank bank = new Bank();
-        return bank.pay(creditCard, personName);
+        return bank.pay(creditCard, personName, amount);
     }
 }
